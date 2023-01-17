fcd8bba25402a22272e1f2af086b58aba55bb33a
marvelous-ikechi <mikechi@bscglobal.com>
2023-01-17 08:56:32 +0100

    Button component updated to accomodate other use-cases.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,21 @@
-export function Button({ children, className }: { children: React.ReactNode; className?: string }) {
+export function Button({
+  children,
+  className,
+  backgroundColor,
+  textColor,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  backgroundColor?: string;
+  textColor?: string;
+}) {
+  const background = backgroundColor ?? 'highlight';
+  const text = textColor ?? 'white';
   return (
     <button
       className={`${
         className || ''
-      } bg-[#e97724] inline-flex rounded-full text-white font-bold uppercase
+      } bg-${background} inline-flex rounded-full text-${text} font-bold uppercase
       text-[14px] leading-[17px] px-8 py-3  whitespace-nowrap
       hover:bg-gradient-to-t from-[#f6c4a1] to-[#e97724] transition-all hover:duration-1000 ease-in-out
       `}
