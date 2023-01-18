f023cac9cec022705810c46f426b6e97592a0125
marvelous-ikechi <mikechi@bscglobal.com>
2023-01-18 16:33:50 +0100

    backgroundColor and text color updated to change if active prop is set to true.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,25 @@
-export function Button({ children, className }: { children: React.ReactNode; className?: string }) {
+import { MouseEventHandler } from 'react';
+
+export function Button({
+  children,
+  className,
+  onClick,
+  active,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: MouseEventHandler;
+  active?: boolean;
+}) {
+  const backgroundColor = active ? 'white' : 'highlight';
+  const textColor = active ? 'highlight' : 'white';
+
   return (
     <button
+      onClick={onClick}
       className={`${
         className || ''
-      } bg-highlight inline-flex rounded-full text-white font-bold uppercase
+      } bg-${backgroundColor} inline-flex rounded-full text-${textColor} font-bold uppercase
       text-[14px] leading-[17px] px-8 py-3  whitespace-nowrap
       hover:bg-gradient-to-t from-[#f6c4a1] to-[#e97724] transition-all hover:duration-1000 ease-in-out
       `}
