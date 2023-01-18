67a648bfc120f66ee0b33649506c38a68444a4de
marvelous-ikechi <mikechi@bscglobal.com>
2023-01-18 13:50:57 +0100

    Button restored to original state without backgroundcolor and textcolor props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,9 @@
-export function Button({
-  children,
-  className,
-  backgroundColor,
-  textColor,
-}: {
-  children: React.ReactNode;
-  className?: string;
-  backgroundColor?: string;
-  textColor?: string;
-}) {
-  const background = backgroundColor ?? 'highlight';
-  const text = textColor ?? 'white';
+export function Button({ children, className }: { children: React.ReactNode; className?: string }) {
   return (
     <button
       className={`${
         className || ''
-      } bg-${background} inline-flex rounded-full text-${text} font-bold uppercase
+      } bg-highlight inline-flex rounded-full text-white font-bold uppercase
       text-[14px] leading-[17px] px-8 py-3  whitespace-nowrap
       hover:bg-gradient-to-t from-[#f6c4a1] to-[#e97724] transition-all hover:duration-1000 ease-in-out
       `}
