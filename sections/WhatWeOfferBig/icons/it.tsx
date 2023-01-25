a17fea6dbc1189ccfbccecd2990fe819a8bc58a8
marvelous-ikechi <mikechi@bscglobal.com>
2023-01-25 12:34:25 +0100

    icons added.

---
+++ sections/WhatWeOfferBig/icons/it.tsx
@@ -0,0 +1,45 @@
+export default (
+  <svg xmlns="http://www.w3.org/2000/svg" width="80" height="129.031" viewBox="0 0 80 129.031">
+    <defs>
+      <clipPath id="clip-path">
+        <rect
+          id="Rectangle_418"
+          data-name="Rectangle 418"
+          width="58.747"
+          height="66.486"
+          fill="none"
+        />
+      </clipPath>
+    </defs>
+    <g id="Component_111_1" data-name="Component 111 – 1" transform="translate(1.115)">
+      <g id="Group_1038" data-name="Group 1038" transform="translate(12.276)" opacity="0.6">
+        <g id="Group_1037" data-name="Group 1037" clip-path="url(#clip-path)">
+          <path
+            id="Path_3030"
+            data-name="Path 3030"
+            d="M46.94,23.615a11.853,11.853,0,1,0-11.473-9.054L19.435,24.236a11.808,11.808,0,1,0,0,18.014l16.032,9.675a11.8,11.8,0,1,0,4.009-6.392L23.335,35.793a11.574,11.574,0,0,0,0-5.1l16.141-9.739a11.756,11.756,0,0,0,7.464,2.66"
+            fill="#fff"
+          />
+        </g>
+      </g>
+      <text
+        id="Holistic_IT_Solutions"
+        data-name="Holistic IT
+Solutions"
+        transform="translate(38.885 106.031)"
+        fill="#fff"
+        font-size="16"
+        font-family="Montserrat-Medium, Montserrat"
+        font-weight="500"
+        opacity="0.6"
+      >
+        <tspan x="-39" y="0">
+          Holistic IT
+        </tspan>
+        <tspan x="-37" y="19">
+          Solutions
+        </tspan>
+      </text>
+    </g>
+  </svg>
+);
