542e8b2bd55b3d7a4244029e15e8a4bba03bb7e5
marvelous-ikechi <mikechi@bscglobal.com>
2023-01-25 15:26:11 +0100

    icons updated to components

---
+++ sections/WhatWeOfferBig/icons/vue.tsx
@@ -0,0 +1,29 @@
+export default (
+  <svg
+    id="vue"
+    xmlns="http://www.w3.org/2000/svg"
+    width="60"
+    height="60"
+    viewBox="0 0 78.102 67.872"
+  >
+    <g id="Group_1010" data-name="Group 1010" opacity="0.6">
+      <path
+        id="Path_2902"
+        data-name="Path 2902"
+        d="M-113.032,0l-9.018,15.674L-131.069,0H-161.1l39.051,67.872L-83,0Z"
+        transform="translate(161.101)"
+        fill="#fff"
+      />
+    </g>
+    <g id="Group_1011" data-name="Group 1011" transform="translate(15.621)">
+      <path
+        id="Path_2903"
+        data-name="Path 2903"
+        d="M-76.3,0-85.32,15.674-94.338,0h-14.412L-85.32,40.722-61.89,0Z"
+        transform="translate(108.751)"
+        fill="#efefef"
+        opacity="0.6"
+      />
+    </g>
+  </svg>
+);
