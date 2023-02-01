248fd62f1be0631680ad73c371d1415b10ab10b8
marvelous-ikechi <mikechi@bscglobal.com>
2023-02-01 13:18:56 +0100

    clients dummy data created

---
+++ public/case-studies/clients.ts
@@ -0,0 +1,38 @@
+import { Props } from '../../components/ClientCard';
+import revio from '../case-studies/revio';
+
+export const clients: Props[] = [
+  {
+    clientName: 'revio',
+    clientLogo: revio,
+    responsibilities: [
+      'System Architecture',
+      'Back-end Development',
+      'Infrastructure Design',
+      'Database Design',
+    ],
+    backgroundImageSrc: '../case-studies/revioBackground.png',
+  },
+  {
+    clientName: 'revio',
+    clientLogo: revio,
+    responsibilities: [
+      'System Architecture',
+      'Back-end Development',
+      'Infrastructure Design',
+      'Database Design',
+    ],
+    backgroundImageSrc: '../case-studies/raubexBackground.png',
+  },
+  {
+    clientName: 'revio',
+    clientLogo: revio,
+    responsibilities: [
+      'System Architecture',
+      'Back-end Development',
+      'Infrastructure Design',
+      'Database Design',
+    ],
+    backgroundImageSrc: '../case-studies/auroraBackground.png',
+  },
+];
