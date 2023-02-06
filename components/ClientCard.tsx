e60277dfe3cf12e5aca8f16be8fc3b627b4291c5
marvelous-ikechi <mikechi@bscglobal.com>
2023-02-06 15:49:57 +0100

    ClientCard wrapped around link

--- components/ClientCard.tsx
+++ components/ClientCard.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import Link from 'next/link';
 
 export interface Props {
   clientName: string;
@@ -9,35 +10,37 @@ export interface Props {
 
 function ClientCard({ clientName, clientLogo, responsibilities, backgroundImageSrc }: Props) {
   return (
-    <div className="flex group  w-full relative flex-1 h-[300px]  flex-col items-center">
-      <div className="hidden  absolute w-full z-10 h-[280px] left-2 opacity-50 bg-black group-hover:block ">
-        <div className="flex flex-col h-[300px]  justify-center px-5 pb-5 ">
-          <div>{clientLogo}</div>
-          <div>
-            {responsibilities.map((item, key) => (
-              <div className="flex flex-col mt-3 " key={key}>
-                <span className="text-white">{item}</span>
-              </div>
-            ))}
-          </div>
-          <div className="flex  items-end justify-end">
-            <a href="#" className="uppercase font-medium text-lg text-highlight">
-              View Project
-            </a>
+    <Link href="/case-studies/details">
+      <div className="flex group  w-full relative flex-1 h-[300px]  flex-col items-center">
+        <div className="hidden  absolute w-full z-10 h-[280px] left-2 opacity-50 bg-black group-hover:block ">
+          <div className="flex flex-col h-[300px]  justify-center px-5 pb-5 ">
+            <div>{clientLogo}</div>
+            <div>
+              {responsibilities.map((item, key) => (
+                <div className="flex flex-col mt-3 " key={key}>
+                  <span className="text-white">{item}</span>
+                </div>
+              ))}
+            </div>
+            <div className="flex  items-end justify-end">
+              <a href="#" className="uppercase font-medium text-lg text-highlight">
+                View Project
+              </a>
+            </div>
           </div>
         </div>
+        <div
+          className=" px-10 w-full z-0 py-10  flex-1 "
+          style={{
+            backgroundImage: `url(/${backgroundImageSrc})`,
+            backgroundColor: 'black',
+            backgroundSize: 'cover',
+            marginBottom: 20,
+            marginLeft: 20,
+          }}
+        ></div>
       </div>
-      <div
-        className=" px-10 w-full z-0 py-10  flex-1 "
-        style={{
-          backgroundImage: `url(/${backgroundImageSrc})`,
-          backgroundColor: 'black',
-          backgroundSize: 'cover',
-          marginBottom: 20,
-          marginLeft: 20,
-        }}
-      ></div>
-    </div>
+    </Link>
   );
 }
 
