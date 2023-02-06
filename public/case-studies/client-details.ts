0e30d9f276185a558c574a45a122e31ea52e8a9f
marvelous-ikechi <mikechi@bscglobal.com>
2023-02-06 21:55:49 +0100

    individual client-items created

---
+++ public/case-studies/client-details.ts
@@ -0,0 +1,71 @@
+export const clientDetails = [
+  {
+    clientName: 'Revio',
+    coverImage: 'revioBackground-2.png',
+    problem:
+      'We joined Revio shortly after their inception to add debit order collections as one of the tools on their system. Integrating with local banking systems offered some very interesting and unique challenges',
+    solution:
+      'We joined Revio shortly after their inception to add debit order collections as one of the tools on their system. Integrating with local banking systems offered some very interesting and unique challenges',
+    challenge: [
+      {
+        title: 'Concept Behind the Product',
+        description:
+          'Direct debits (or debit orders) are generally a foreign concept for most international clients, but for medium to large companies in South Africa this is the most effective tool for collecting insurance premiums, rent, car payments, etc. on a large scale.',
+      },
+      {
+        title: 'Gap in the Market',
+        description:
+          'Give smaller businesses the opportunity to affordably play within the debit order collection space. Handle large amounts of collections for larger businesses. Abstract banking processes for clients. Transparency and early warning on negative collection results.',
+      },
+      {
+        title: 'The Problem',
+        description:
+          'Easier way for clients to utilise the full suite of collection tools provided by the banks. Mandate management (Create, Amendments and Authentication). Automatic recon and settlement from intermediary accounts.',
+      },
+    ],
+    services: [
+      'System Architecture',
+      'Database Design',
+      'Infrastructure Design',
+      'Back-end Development',
+    ],
+    teamSize: 3,
+    quoteSectionImage: '/../public/case-studies/clay.png',
+    infoGathered: [
+      'What were the banks’ limitations? Which technologies do they use?',
+      'What was our average amount of projected collections? Per day? Per month?',
+      'Are we able to find a better way to handle the banks’ file-based systems?',
+      'How can we be better than the competitions’ systems?',
+      'How do we simplify some of the concepts?',
+      'How do we secure the whole process?',
+      'How do we test the system?',
+    ],
+    infoGatheringActivities: [
+      ' Going through bank integration specification documents',
+      'Speaking to stakeholders',
+      'Researching the tech available on cloud platforms such as AWS',
+      'Looking at best practices for enterprise systems',
+    ],
+    designProcess: [
+      'Talking to stakeholders to ask about possible pitfalls and known hiccups',
+      'Created multiple iterations of entity relationship diagrams (ERD) to flesh out possible database structures.',
+      'Drafted many flow diagrams to determine the lifecycle of a collection or mandate.',
+      'Discussed which technologies to use to solve problems created by older banking technologies.',
+      'Discuss ways to test system without actually connecting to the bank',
+    ],
+    buildingProcess: [
+      'Talking to stakeholders to ask about possible pitfalls and known hiccups.',
+      'Created multiple iterations of entity relationship diagrams (ERD) to flesh out possible database structures.',
+      'Drafted many flow diagrams to determine the lifecycle of a collection or mandate.',
+      'Discussed which technologies to use to solve problems created by older banking technologies.',
+      'Discuss ways to test system without actually connecting to the bank',
+    ],
+    results: [
+      'Mandate management made easy. Intuitive creation, authentication and amendment calls.',
+      'Able to create collections from existing mandates to save time.',
+      'Able to handle the processing of collections end-to-end.',
+      'Comprehensive testing and monitoring',
+    ],
+    clientResults: '50 Contracted Clients at Present',
+  },
+];
