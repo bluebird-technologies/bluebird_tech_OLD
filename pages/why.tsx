a4092ee0baf2afbc4fee0a000932524d2cb66cf5
ShaunKearley <shaun@bluebird.dev>
2023-03-02 12:31:05 +0200

    Reworking why bluebird page, first section

--- pages/why.tsx
+++ pages/why.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Container } from '../components/Container';
+import React, { useState } from 'react';
 import { Header } from '../components/Header';
-import UnderlineSvg from '../public/why/underline.svg';
-import MissionSvg from '../public/why/mission.svg';
-import VisionSvg from '../public/why/vision.svg';
-import Image from 'next/image';
+import underline from '../components/WhyBluebird/icons/underline';
+import orangeCircle from '../components/WhyBluebird/icons/orange-circle';
+import orangeCircleSelected from '../components/WhyBluebird/icons/orange-circle-selected';
+import blueCircle from '../components/WhyBluebird/icons/blue-circle';
+import blueCircleSelected from '../components/WhyBluebird/icons/blue-circle-selected';
 import { Button } from '../components/Button';
 import WaveLines from '../public/why/wave-lines.svg';
 import Lottie from 'lottie-react';
@@ -13,6 +13,7 @@ import leanAnimation from '../public/why/leanAnimation.json';
 import personalAnimation from '../public/why/personalAnimation.json';
 import Line from '../public/why/line.svg';
 import Footer from '../components/Footer';
+import { CircleButton } from '../components/WhyBluebird/CircleButton';
 
 const Why = () => {
   type btnOptions = 'Lean' | 'Boutique' | 'Personal';
@@ -21,7 +22,7 @@ const Why = () => {
   const coreValues: {
     key: btnOptions;
     description: string;
-    animation: any;
+    animation: unknown;
   }[] = [
     {
       key: 'Boutique',
@@ -43,62 +44,99 @@ const Why = () => {
     },
   ];
 
+  const [isFirstSelected, setIsFirstSelected] = useState(true);
+
   return (
-    <div className=" flex-1 flex flex-col bg-seashell h-[100%]">
-      <div
-        style={{
-          width: '100%',
-          height: '430px',
-          position: 'relative',
-          backgroundImage: `url(/contact/darkBackground.svg)`,
-          backgroundRepeat: 'no-repeat',
-        }}
-        className=" flex-1 pb-24 items-center "
-      >
+    <div>
+      <div className="w-full flex flex-col relative h-[110vh] xl:h-[94vh] ">
+        <div
+          className="bg-primary h-full"
+          style={{
+            position: 'absolute',
+            left: '-16%',
+            width: '130vw',
+            borderBottomLeftRadius: '50%',
+            borderBottomRightRadius: '50%',
+            zIndex: -1,
+          }}
+        />
         <Header />
-        <Container wide center row>
-          <div className="  w-full items-center  justify-center">
-            <div className="flex flex-row ">
-              <h1 className="font-bold text-white text-5xl">WHY</h1>
-              <h2 className="font-bold text-white text-5xl ml-2 italic">BLUEBIRD ?</h2>
-            </div>
-            <div className=" w-2/5 flex  items-left justify-end">
-              <UnderlineSvg />
-            </div>
+
+        {/* Heading section */}
+        <div
+          className="flex flex-col justify-center xl:justify-start mt-24 px-12 xl:px-48 2xl:px-60"
+          style={{
+            width: 'fit-content',
+            blockSize: 'fit-content',
+          }}
+        >
+          <div className="font-bold text-white text-5xl">
+            WHY <span className="italic">BLUEBIRD ?</span>
           </div>
-        </Container>
-        <Container wide center row>
-          <div className="mt-3 flex flex-row items-center justify-between flex-1">
-            <div className="">
-              <div className="flex flex-row items-center">
-                <Image src="/why/laptop.png" alt="laptop" width={500} height={500} />
-                <div>
-                  <div className="relative flex flex-row items-center ">
-                    <div className="absolute -left-5 bottom-6">
-                      <VisionSvg />
-                    </div>
+          <div className="flex justify-end">
+            <div className="w-[250px]">{underline}</div>
+          </div>
+        </div>
+
+        {/* post heading section */}
+        <div className="flex flex-col xl:flex-row items-center justify-center xl:justify-start mt-12 px-12 xl:px-48 2xl:px-60">
+          <div className="w-1/2 relative pr-0 2xl:pr-16">
+            <picture>
+              <source srcSet="/why/laptop_1.png" />
+              <img src="/why/laptop_1.png" alt="laptop" />
+            </picture>
+            <div className="absolute left-0 top-0 ml-[26vw] 2xl:mt-8 lg:mt-4 2xl:scale-100 lg:scale-90">
+              <div className="flex flex-col space-y-12">
+                {isFirstSelected ? (
+                  <div className="pl-16">
+                    <CircleButton disabled icon={orangeCircleSelected} text="VISION" />
                   </div>
-                  <div className="relative">
-                    <div className=" absolute -left-20 ">
-                      <MissionSvg />
-                    </div>
+                ) : (
+                  <div className="pl-16">
+                    <CircleButton
+                      onClick={() => setIsFirstSelected((prev) => !prev)}
+                      icon={orangeCircle}
+                      text="VISION"
+                    />
                   </div>
-                </div>
+                )}
+                {!isFirstSelected ? (
+                  <CircleButton disabled icon={blueCircleSelected} text="MISSION" />
+                ) : (
+                  <CircleButton
+                    onClick={() => setIsFirstSelected((prev) => !prev)}
+                    icon={blueCircle}
+                    text="MISSION"
+                  />
+                )}
               </div>
             </div>
-            <div className="flex flex-row w-2/5">
-              <h1 className="text-white">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-                eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                takimata sanctus est Lorem ipsum dolor sit amet.
-              </h1>
+          </div>
+          <div className="w-1/2 mt-12 2xl:mt-0">
+            <div className="flex w-full 2xl:justify-start justify-center">
+              <div className="hidden xl:block w-1/3" />
+              {isFirstSelected ? (
+                <p className="text-white text-xl w-full xl:w-2/3">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                  tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                  vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                  no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+              ) : (
+                <p className="text-white text-xl w-full xl:w-2/3">
+                  Lorem ipsum dolor sit amet sed diam nonumy eirmod tempor invidunt ut labore et
+                  dolore magna aliquyam erat, sed diam voluptua. Sed diam nonumy eirmod tempor
+                  invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
+                  et accusam et justo duo dolores et ea rebum.
+                </p>
+              )}
             </div>
           </div>
-        </Container>
+        </div>
       </div>
-      <div className="mx-32">
-        <div className="flex flex-col w-full ">
+
+      <div className="mx-32 mt-12">
+        <div className="flex flex-col w-full">
           <div>
             <div className="flex mb-4 flex-1 items-center justify-center">
               <h1 className="text-center font-bold italic text-3xl text-secondary">
@@ -163,7 +201,7 @@ const Why = () => {
               <div className="flex flex-1 items-center justify-center">
                 <h1 className="text-white text-center text-3xl font-bold">CULTURE</h1>
               </div>
-              <div className="relative mx-10 mt-14 flex  justify-around">
+              {/* <div className="relative mx-10 mt-14 flex  justify-around">
                 <Image
                   src={'/why/handshake.png'}
                   alt="handshake"
@@ -200,7 +238,7 @@ const Why = () => {
                     top: 90,
                   }}
                 />
-              </div>
+              </div> */}
               <div className="px-8 flex-1 mt-24 items-center justify-center">
                 <h1 className="text-white  text-center font-bold">
                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
@@ -216,7 +254,7 @@ const Why = () => {
       <div className="px-8 flex flex-1 py-8  items-center flex-col bg-primary justify-center">
         <h1 className="text-highlight font-bold text-3xl">AWARDS</h1>
         <Line />
-        <Image
+        {/* <Image
           src={'/why/topDevelopers.png'}
           height={200}
           alt="top-software-developers"
@@ -224,7 +262,7 @@ const Why = () => {
           style={{
             marginTop: 20,
           }}
-        />
+        /> */}
       </div>
       <Footer />
     </div>
