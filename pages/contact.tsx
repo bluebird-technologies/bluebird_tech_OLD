66fc288bb4e467e43ef4729b390f706cb36eda30
marvelous-ikechi <mikechi@bscglobal.com>
2023-03-09 12:15:49 +0100

    font-size update

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -41,49 +41,29 @@ const Contact = () => {
       <div className="flex flex-row px-20 justify-center items-center bg-secondary py-32 -mt-32">
         <form className="flex flex-1 my-8 flex-col items-center justify-center">
           <input
-            className=" my-4 px-3 font-normal rounded-[20px] h-12 w-[518px]"
+            className="py-4 my-4 px-3 text-lg rounded-[20px]  w-[518px]"
             type={'text'}
             placeholder={'Your name'}
           />
           <input
-            className=" my-4 px-3 font-normal rounded-[20px] h-12 w-[518px]"
+            className=" py-4 my-4 px-3 font-normal rounded-[20px] text-lg  w-[518px]"
             type={'text'}
             placeholder={'Your company (optional)'}
           />
-          {/* <div className="flex h-12 my-4  bg-white items-center w-[518px] rounded-[20px]">
-            <select
-              title="options"
-              placeholder="Reason for contacting us (select)"
-              className="mx-3 font-medium h-10 opacity-1 w-[518px] border-0 focus:outline-none"
-              id="contact-select"
-              style={{
-                appearance: 'none',
-                borderRadius: 20,
-              }}
-            >
-              <option disabled selected value="">
-                Reason for contacting us (select)
-              </option>
-              <option value="option1">In need of development work</option>
-              <option value="option2">Apply for a job</option>
-              <option value="option3">Outstaffing</option>
-              <option value="option3">Formal Quote</option>
-              <option value="option3">General Enquiry</option>
-            </select>
-          </div> */}
           <SelectDropdown
             placeholder="Reason for contacting us(select)"
             options={dropdownOptions}
+            selectedItem={(option) => console.log(option)}
           />
           <input
-            className=" my-4 px-3 rounded-[20px] h-12 w-[518px]"
+            className=" my-4 px-3 rounded-[20px] text-lg py-4 w-[518px]"
             type={'email'}
             placeholder={'Your email'}
           />
           <textarea
             rows={10}
             cols={50}
-            className="w-[518px] my-4 px-3 py-4 rounded-[22px] min-h-10"
+            className="w-[518px] my-4 px-3 py-4 text-lg rounded-[22px] min-h-10"
             placeholder="Your message"
           />
           <Button>Send</Button>
