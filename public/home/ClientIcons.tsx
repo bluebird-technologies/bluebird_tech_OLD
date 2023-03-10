ff4c11266a817a8a60d56813bcd22a686d926177
marvelous-ikechi <mikechi@bscglobal.com>
2023-03-10 10:24:51 +0100

    client icon hardcoded

--- public/home/ClientIcons.tsx
+++ public/home/ClientIcons.tsx
@@ -1,80 +1,78 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import { clientIcons } from '../../components/clientIcons';
 
 export default function ClientIcons() {
   return (
-    <div className="w-full">
-      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 grid-flow-col lg:grid-cols-4 gap-3">
-        {clientIcons.slice(0, 4).map((fileName, index) => (
-          <div key={index} className="flex items-center justify-center mb-20 rounded-lg p-4">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/client-icons/${fileName}`}
-              alt={fileName}
-              className="max-w-full max-h-full"
-            />
-          </div>
-        ))}
+    <div className="w-full h-full">
+      <div className="flex flex-row items-center my-8  justify-between">
+        <img src={'/client-icons/22seven.svg'} alt={'22seven'} className="max-w-full max-h-full" />
+        <img
+          src={'/client-icons/bankTech.svg'}
+          alt={'bankTech'}
+          className="max-w-full  max-h-full"
+        />
+        <img src={'/client-icons/revix.svg'} alt={'revix'} className="max-w-full max-h-full" />
+        <img src={'/client-icons/layup.svg'} alt={'layup'} className="max-w-full max-h-full" />
       </div>
-      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 grid-flow-col lg:grid-cols-4 gap-3">
-        {clientIcons.slice(4, 7).map((fileName, index) => (
-          <div key={index} className="flex items-center justify-center mb-20 rounded-lg p-4">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/client-icons/${fileName}`}
-              alt={fileName}
-              className="max-w-full max-h-full"
-            />
-          </div>
-        ))}
+      <div className="flex flex-row items-center my-8 px-8 justify-between">
+        <img src={'/client-icons/duepay.svg'} alt={'duepay'} className="max-w-full max-h-full" />
+        <img
+          src={'/client-icons/doctorly.svg'}
+          alt={'doctorly'}
+          className="max-w-full max-h-full"
+        />
+        <img
+          src={'/client-icons/thorchain.svg'}
+          alt={'thorchain'}
+          className="max-w-full max-h-full"
+        />
       </div>
-      <div className="grid grid-cols-1 mb-6 sm:grid-cols-2 md:grid-cols-3 grid-flow-col lg:grid-cols-4 gap-3">
-        {clientIcons.slice(7, 11).map((fileName, index) => (
-          <div key={index} className="flex items-center justify-center mb-20  rounded-lg p-4">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/client-icons/${fileName}`}
-              alt={fileName}
-              className="max-w-full max-h-full"
-            />
-          </div>
-        ))}
+      <div className="flex flex-row items-center my-8 justify-between ">
+        <img
+          src={'/client-icons/theDelta.svg'}
+          alt={'theDelta'}
+          className="max-w-full max-h-full"
+        />
+        <img src={'/client-icons/raubex.svg'} alt={'raubex'} className="max-w-full max-h-full" />
+        <img
+          src={'/client-icons/pump_abraison.svg'}
+          alt={'pump_abraison'}
+          className="max-w-full max-h-full"
+        />
+        <img
+          src={'/client-icons/coinDirect.svg'}
+          alt={'coinDirect'}
+          className="max-w-full max-h-full"
+        />
       </div>
-      <div className="grid grid-cols-1 mb-6 sm:grid-cols-2 md:grid-cols-3 grid-flow-col lg:grid-cols-4 gap-3">
-        {clientIcons.slice(11, 15).map((fileName, index) => (
-          <div key={index} className="flex items-center justify-center mb-20  rounded-lg p-4">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/client-icons/${fileName}`}
-              alt={fileName}
-              className="max-w-full max-h-full hover:opacity-75"
-            />
-          </div>
-        ))}
+      <div className="flex flex-row items-center my-8 justify-between ">
+        <img src={'/client-icons/bvnk.svg'} alt={'bvnk'} className="max-w-full max-h-full" />
+        <img src={'/client-icons/craft.svg'} alt={'craft'} className="max-w-full max-h-full" />
+        <img src={'/client-icons/pepsi.svg'} alt={'pepsi'} className="max-w-full max-h-full" />
+        <img src={'/client-icons/breez.svg'} alt={'breez'} className="max-w-full max-h-full" />
       </div>
-      <div className="grid grid-cols-1 mb-6 sm:grid-cols-2 md:grid-cols-3 grid-flow-col lg:grid-cols-3 gap-3">
-        {clientIcons.slice(15, 18).map((fileName, index) => (
-          <div key={index} className="flex items-center justify-center mb-20  rounded-lg p-4">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/client-icons/${fileName}`}
-              alt={fileName}
-              className="max-w-full max-h-full"
-            />
-          </div>
-        ))}
+      <div className="flex flex-row items-center my-8 px-8 justify-between ">
+        <img
+          src={'/client-icons/greensun.svg'}
+          alt={'greensun'}
+          className="max-w-full max-h-full"
+        />
+        <img src={'/client-icons/bsc.svg'} alt={'bsc'} className="max-w-full max-h-full" />
+        <img
+          src={'/client-icons/polygonProp.svg'}
+          alt={'polygonProp'}
+          className="max-w-full max-h-full"
+        />
       </div>
-      <div className="grid grid-cols-1 mb-6 sm:grid-cols-2 md:grid-cols-3 grid-flow-col lg:grid-cols-4 gap-3">
-        {clientIcons.slice(18, 22).map((fileName, index) => (
-          <div key={index} className="flex items-center justify-center mb-20  rounded-lg p-4">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/client-icons/${fileName}`}
-              alt={fileName}
-              className="max-w-full max-h-full"
-            />
-          </div>
-        ))}
+      <div className="flex flex-row items-center my-8 justify-between ">
+        <img src={'/client-icons/overlap.svg'} alt={'overlap'} className="max-w-full max-h-full" />
+        <img
+          src={'/client-icons/ambercircle.svg'}
+          alt={'ambercircle'}
+          className="max-w-full max-h-full"
+        />
+        <img src={'/client-icons/asklulo.svg'} alt={'asklulo'} className="max-w-full max-h-full" />
+        <img src={'/client-icons/tweak.svg'} alt={'tweak'} className="max-w-full max-h-full" />
       </div>
     </div>
   );
