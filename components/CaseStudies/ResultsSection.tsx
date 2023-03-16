1943e7f50417a43774ec98523029798e1885d1b3
ShaunKearley <shaun@bluebird.dev>
2023-03-16 15:33:35 +0200

    Remove redundant file, justify text to middle

--- components/CaseStudies/ResultsSection.tsx
+++ components/CaseStudies/ResultsSection.tsx
@@ -12,22 +12,22 @@ export function ResultsSection({
   results,
 }: ResultsSectionProps) {
   return (
-    <div className="flex flex-1 flex-row ">
+    <div className="flex flex-1 flex-row h-[620px]">
       <div
         style={{
           backgroundImage: `url(${resultsBackgroundImageString})`,
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
         }}
-        className="flex flex-1 h--[620px] bg-black"
+        className="flex flex-1 bg-black"
       >
-        <div className="flex items-end justify-end ">
+        <div className="flex items-end justify-end">
           <div className="flex bg-highlight ml-16 mb-20 flex-wrap py-4 px-8 w-[500px] items-center justify-center rounded-full">
             <span className="text-white font-medium text-2xl text-center">{clientResults}</span>
           </div>
         </div>
       </div>
-      <div className="bg-secondary flex w-2/5 flex-col flex-wrap px-16 py-7">
+      <div className="bg-secondary flex w-2/5 flex-col flex-wrap px-16 py-7 justify-center">
         <h1 className="text-white text-4xl uppercase font-bold my-4">Results</h1>
         <ul className=" pb-4">
           {results.map((item, key) => (
