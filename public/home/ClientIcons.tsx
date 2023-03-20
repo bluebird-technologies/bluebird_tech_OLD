53b53a654b43dafed05281328ad149ba379723b5
marvelous-ikechi <mikechi@bscglobal.com>
2023-03-20 11:45:38 +0100

    add hover state to client icons

--- public/home/ClientIcons.tsx
+++ public/home/ClientIcons.tsx
@@ -4,30 +4,33 @@ import Link from 'next/link';
 
 export default function ClientIcons() {
   return (
-    <div className="w-full h-full">
-      <div className="flex flex-row items-center mb-14  justify-between">
-        <Link href={'https://www.22seven.com'}>
+    <div className="w-full opac h-full">
+      <div className="flex flex-row items-center mb-14  justify-between ">
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.22seven.com'}
+        >
           <div>
             <picture>
               <img src="/client-icons/22seven.svg" alt="22seven" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://bank.tech'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://bank.tech'}>
           <div>
             <picture>
               <img src="/client-icons/bankTech.svg" alt="bank Tech" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://revix.com'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://revix.com'}>
           <div>
             <picture>
               <img src="/client-icons/revix.svg" alt="revix" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://layup.co.za'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://layup.co.za'}>
           <div>
             <picture>
               <img src="/client-icons/layup.svg" alt="layup" />
@@ -36,21 +39,27 @@ export default function ClientIcons() {
         </Link>
       </div>
       <div className="flex flex-row items-center mb-14  px-12 justify-between">
-        <Link href={'https://www.duepay.co.za'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.duepay.co.za'}
+        >
           <div>
             <picture>
               <img src="/client-icons/duepay.svg" alt="duepay" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.doctorly.de/en'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.doctorly.de/en'}
+        >
           <div>
             <picture>
               <img src="/client-icons/doctorly.svg" alt="doctorly" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://thorchain.org'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://thorchain.org'}>
           <div>
             <picture>
               <img src="/client-icons/thorchain.svg" alt="thorchain" />
@@ -59,28 +68,37 @@ export default function ClientIcons() {
         </Link>
       </div>
       <div className="flex flex-row items-center mb-14  justify-between ">
-        <Link href={'https://www.thedelta.io'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.thedelta.io'}
+        >
           <div>
             <picture>
               <img src="/client-icons/theDelta.svg" alt="theDelta" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://raubex.com'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://raubex.com'}>
           <div>
             <picture>
               <img src="/client-icons/raubex.svg" alt="raubex" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.pabtglobal.com/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.pabtglobal.com/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/pump_abrasion.svg" alt="Pump and abrasion" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.coindirect.com'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.coindirect.com'}
+        >
           <div>
             <picture>
               <img src="/client-icons/coindirect.svg" alt="Coin Direct" />
@@ -89,28 +107,34 @@ export default function ClientIcons() {
         </Link>
       </div>
       <div className="flex flex-row items-center mb-14  px-12 justify-between ">
-        <Link href={'https://bvnk.com/'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://bvnk.com/'}>
           <div>
             <picture>
               <img src="/client-icons/bvnk.svg" alt="Bvnk" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.craft.financial/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.craft.financial/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/craft.svg" alt="Craft" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.pepsi.co.za/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.pepsi.co.za/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/pepsi.svg" alt="Pepsi" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://breez.insure'}>
+        <Link className="bg-[#f1f1f1] opacity-50 hover:opacity-100 " href={'https://breez.insure'}>
           <div>
             <picture>
               <img src="/client-icons/breez.svg" alt="Breez" />
@@ -119,21 +143,30 @@ export default function ClientIcons() {
         </Link>
       </div>
       <div className="flex flex-row items-center mb-14  px-12 justify-between ">
-        <Link href={'https://greensun.co.za/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://greensun.co.za/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/greensun.svg" alt="Greensun" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://bscglobal.com/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://bscglobal.com/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/bsc.svg" alt="Bsc global" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.polygonprop.com/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.polygonprop.com/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/polygonprop.svg" alt="Polygon prop" />
@@ -142,28 +175,40 @@ export default function ClientIcons() {
         </Link>
       </div>
       <div className="flex flex-row items-center   mb-14  justify-between ">
-        <Link href={'https://www.overlap-app.com'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.overlap-app.com'}
+        >
           <div>
             <picture>
               <img src="/client-icons/overlap.svg" alt="Overlap" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://ambercircle.co.za/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://ambercircle.co.za/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/ambercircle.svg" alt="Ambercircle" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.asklulo.com/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.asklulo.com/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/asklulo.svg" alt="Asklulo" />
             </picture>
           </div>
         </Link>
-        <Link href={'https://www.tweakcarbon.com/'}>
+        <Link
+          className="bg-[#f1f1f1] opacity-50 hover:opacity-100 "
+          href={'https://www.tweakcarbon.com/'}
+        >
           <div>
             <picture>
               <img src="/client-icons/tweak.svg" alt="Pump and abrasion" />
