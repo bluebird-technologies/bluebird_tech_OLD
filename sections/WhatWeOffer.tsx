4a3a1cfcc6342d83b13de439ee466f17c880ba6b
marvelous-ikechi <mikechi@bscglobal.com>
2023-03-29 12:50:34 +0100

    responsiveness added to WhatWeOffer component.

--- sections/WhatWeOffer.tsx
+++ sections/WhatWeOffer.tsx
@@ -31,25 +31,34 @@ const items = [
 
 export function WhatWeOffer() {
   return (
-    <div className="flex flex-wrap -mx-[16px] gap-y-[32px] mt-[78px] mb-[91px]">
-      <div className="w-1/4 px-[20px] flex items-center">
-        <h2 className="title-2 secondary">WHAT WE OFFER</h2>
+    <div className="mt-[78px] mb-[91px] flex w-full flex-1 flex-col items-center justify-center 2xl:-mx-[16px] 2xl:flex-row  2xl:flex-wrap 2xl:gap-y-[32px] ">
+      <div className="mb-4 flex items-center  2xl:mb-0 2xl:w-1/4 2xl:px-[20px]">
+        <h2 className="title-2 secondary text-center">WHAT WE OFFER</h2>
       </div>
       {items.map((i, index) => (
-        <div key={index} className="w-1/4">
-          <div className="mx-[16px] shadow-[2px_4px_16px_#00000029] pt-[22px] pb-[18px] px-[20px] rounded-[25px] hover:-mt-2 transition-all">
-            <div className="w-[54px] h-[54px] rounded-full shadow-[2px_4px_16px_#00000029] flex items-center justify-center p-3">
+        <div
+          key={index}
+          className="mt-4 w-[300px] items-center justify-center 2xl:mx-0 2xl:mt-0 2xl:w-1/4"
+        >
+          <div className="flex flex-row items-center justify-start rounded-[25px] px-[20px] pt-[22px] pb-[18px] shadow-[2px_4px_16px_#00000029] transition-all hover:-mt-2 2xl:mx-[16px] 2xl:flex-col 2xl:items-start">
+            <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full p-3  shadow-[2px_4px_16px_#00000029]   ">
               <picture>
                 <source srcSet={`/what-we-offer/${i.icon}.svg`} type="image/svg+xml" />
-                <img src={`/what-we-offer/${i.icon}.svg`} alt="Bluebird" className="h-full" />
+                <img
+                  src={`/what-we-offer/${i.icon}.svg`}
+                  alt="Bluebird"
+                  className="h-[30px] w-[30px] 2xl:h-full 2xl:w-full"
+                />
               </picture>
             </div>
-            <div className="text-primary text-[20px] leading-[25px] font-extrabold uppercase mt-[14px]">
+            <div className="ml-2 font-extrabold uppercase leading-[25px] text-primary 2xl:ml-0 2xl:mt-[14px] 2xl:text-[20px]">
               {i.label.split(' ').map((t, i) => (
                 <div key={i}>{t}</div>
               ))}
             </div>
-            <div className="mt-[13px] text-highlight text-[17px] leading-[20px]">MORE INFO</div>
+            <div className="mt-[13px] hidden text-[17px] leading-[20px] text-highlight 2xl:block">
+              MORE INFO
+            </div>
           </div>
         </div>
       ))}
