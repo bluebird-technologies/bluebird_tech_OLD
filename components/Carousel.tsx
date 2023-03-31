252aea29d93f75683f76ee6f40b331cbbdd66283
marvelous-ikechi <mikechi@bscglobal.com>
2023-03-31 09:50:46 +0100

    Carousel updated to be a reusable component.

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,25 +2,15 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-export default function SolutionCarousel() {
-  const slides = [
-    {
-      title: 'Boutique',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    },
-    {
-      title: 'Lean',
-      content:
-        ' Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
-    },
-    {
-      title: 'Personal',
-      content:
-        ' Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    },
-  ];
+interface Props {
+  item: {
+    label?: string;
+    icon?: React.ReactNode;
+    content?: string;
+  }[];
+}
 
+export default function SolutionCarousel({ item }: Props) {
   return (
     <Carousel
       emulateTouch
@@ -34,10 +24,10 @@ export default function SolutionCarousel() {
       centerSlidePercentage={100}
       onChange={(val) => console.log(val)}
     >
-      {slides.map((slide, i) => {
+      {item?.map((slide, i) => {
         return (
           <div key={i}>
-            <CarouselSlideItem title={slide?.title} content={slide.content} />
+            <CarouselSlideItem label={slide?.label} content={slide?.content} icon={slide?.icon} />
           </div>
         );
       })}
@@ -46,23 +36,29 @@ export default function SolutionCarousel() {
 }
 
 function CarouselSlideItem({
-  title,
+  label,
   icon,
   content,
 }: {
-  title?: string;
+  label?: string;
   icon?: React.ReactNode;
-  content: string;
+  content?: string;
 }) {
   return (
     <div className="flex h-96  flex-col items-center  bg-grey  py-4 text-primary">
       <div className=" flex-wrap items-center justify-center text-2xl font-extrabold uppercase leading-[64px]  text-orange">
-        <h1 className="text-center">{title}</h1>
+        <h1 className="text-center">{label}</h1>
       </div>
       <div className="mb-4 w-72 items-center justify-center md:w-auto">
         <h1 className="text-center">{content}</h1>
       </div>
-      <div>{icon}</div>
+      <div>
+        {icon && (
+          <picture>
+            <img src={`${icon}`} alt={`${icon}`} />
+          </picture>
+        )}
+      </div>
     </div>
   );
 }
