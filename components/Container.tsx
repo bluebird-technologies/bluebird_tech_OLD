6fd62022f5642203417b74d872743a65f296b52d
marvelous-ikechi <mikechi@bscglobal.com>
2023-03-31 13:30:15 +0100

    tailwind styles reordered

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,11 +10,11 @@ export function Container({
   row?: boolean;
 }) {
   return (
-    <div className="w-full flex">
+    <div className="flex w-full">
       <div
-        className={`mx-auto relative ${wide ? 'max-w-[1280px]' : 'max-w-[1080px]'} ${
+        className={`relative mx-auto ${wide ? 'max-w-[1280px]' : 'max-w-[1080px]'} ${
           center ? 'items-center' : 'items-start'
-        } px-8 w-full flex ${row ? 'flex-row' : 'flex-col'}`}
+        } flex w-full px-8 ${row ? 'flex-row' : 'flex-col'}`}
       >
         {children}
       </div>
