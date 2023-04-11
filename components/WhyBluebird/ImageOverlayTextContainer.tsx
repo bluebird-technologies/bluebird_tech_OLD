9994889518d83da8676a8dbe6687ce6bb8701314
marvelous-ikechi <mikechi@bscglobal.com>
2023-04-11 08:41:59 +0100

    image overlay hidden for mobile

--- components/WhyBluebird/ImageOverlayTextContainer.tsx
+++ components/WhyBluebird/ImageOverlayTextContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function ImageOverlayTextContainer({ text }: { text: string }) {
   return (
-    <div className="hidden group-hover:block w-[300px] h-[300px] text-highlight text-6xl font-bold uppercase">
+    <div className="xl:hidden xl:group-hover:block w-[300px] h-[300px] text-highlight text-3xl sm:text-4xl xl:text-6xl font-bold uppercase">
       {text}
     </div>
   );
