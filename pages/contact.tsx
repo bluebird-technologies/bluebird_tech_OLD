1a3c3b146b3c76c52fc191c4da054419e6ad0a4a
marvelous-ikechi <mikechi@bscglobal.com>
2023-04-12 12:38:39 +0100

    responsiveness added to screen;
    font sizes adjusted based on screen width

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -20,11 +20,8 @@ const Contact = () => {
     <div className="bg-secondary flex-1 flex flex-col h-full overflow-hidden">
       <div className="relative w-full bg-cover bg-bottom flex flex-col items-center pb-[90px]">
         <div
-          className="bg-primary h-full"
+          className="absolute bg-primary h-full w-[285vw] -z-1 left-[-98%]  2xl:left-[-16%] 2xl:w-[130vw]"
           style={{
-            position: 'absolute',
-            left: '-16%',
-            width: '130vw',
             borderBottomLeftRadius: '50%',
             borderBottomRightRadius: '50%',
           }}
@@ -38,25 +35,25 @@ const Contact = () => {
 
         <div className="flex z-1 relative flex-col mt-20 items-center">
           <h3 className="text-[27px] font-mediumitalic italic text-highlight">Get in Touch</h3>
-          <span className="uppercase mt-4 text-white text-4xl font-extrabold 2xl:text-[52px]">
+          <span className="uppercase my-4 text-white text-4xl font-extrabold 2xl:text-[52px]">
             CONTACT <span className="italic">US</span>
           </span>
           <Swiggle />
         </div>
       </div>
-      <div className="flex flex-row px-20 justify-center items-center pb-32">
-        <form className="flex flex-1 my-8 flex-col items-center justify-center">
+      <div className="flex  flex-row lg:px-20 justify-center  lg:mx-8 items-center pb-32">
+        <form className="flex w-full flex-1 my-8  flex-col items-center justify-center">
           <input
-            className="py-4 my-3 px-4 text-lg rounded-[20px]  w-[518px]"
+            className="w-5/6 py-4 my-3 px-4 text-base md:text-lg rounded-[20px]  lg:w-[518px]"
             type={'text'}
             placeholder={'Your name'}
           />
           <input
-            className=" py-4 my-3 px-4 font-normal rounded-[20px] text-lg  w-[518px]"
+            className=" py-4 my-3 px-4 font-normal rounded-[20px] w-5/6 text-base md:text-lg  lg:w-[518px]"
             type={'text'}
             placeholder={'Your company (optional)'}
           />
-          <div>
+          <div className="w-full  item-center flex justify-center">
             <SelectDropdown
               placeholder="Reason for contacting us(select)"
               options={dropdownOptions}
@@ -64,42 +61,42 @@ const Contact = () => {
             />
           </div>
           <input
-            className=" my-3 px-4 rounded-[20px] text-lg py-4 w-[518px]"
+            className=" my-3 px-4 rounded-[20px] text-base md:text-lg py-4 w-5/6 lg:w-[518px]"
             type={'email'}
             placeholder={'Your email'}
           />
           <textarea
             rows={5}
             cols={50}
-            className="w-[518px] my-3 px-4 py-4 text-lg rounded-[22px] min-h-10 resize-none"
+            className="w-5/6 lg:w-[518px] my-3 px-4 py-4 text-base md:text-lg rounded-[22px] min-h-10 resize-none"
             placeholder="Your message"
           />
           <Button className="w-40 flex flex-1 items-center justify-center text-lg mt-8">
             Send
           </Button>
         </form>
-        <Lottie
-          animationData={Card}
-          loop={true}
-          style={{
-            height: 500,
-            width: 500,
-          }}
-          width={30}
-          height={30}
-        />
+        <div className="hidden lg:block">
+          <Lottie
+            animationData={Card}
+            loop={true}
+            style={{
+              height: 500,
+              width: 500,
+            }}
+            width={30}
+            height={30}
+          />
+        </div>
       </div>
-      <div className="relative w-full flex flex-col items-center justify-center pt-[140px] pb-[90px] -mt-32">
+      <div className="relative w-full flex flex-col items-center justify-center pt-[140px] pb-[90px] -mt-42">
         <div
-          className="bg-[#F1F1F1] h-full"
+          className="absolute left-[-98%] h-full w-[280vw] bg-[#F1F1F1]  lg:left-[-20%] lg:w-[140vw]   xl:left-[-16%] xl:w-[130vw]"
           style={{
-            position: 'absolute',
-            left: '-16%',
-            width: '130vw',
             borderTopLeftRadius: '50%',
             borderTopRightRadius: '50%',
           }}
         />
+
         <div className="z-10 w-full">
           <Footer />
         </div>
