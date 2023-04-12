37720d59a39ca742c2af9552f3f60cfd659202ed
ShaunKearley <shaun@bluebird.dev>
2023-04-12 12:39:52 +0200

    Clear up unused file

--- components/CaseStudies/icons/list-dot.tsx
+++
@@ -1,5 +0,0 @@
-export default (
-  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 50 50">
-    <circle id="Dot" data-name="Dot" cx="25" cy="25" r="20" fill="#fff" />
-  </svg>
-);
