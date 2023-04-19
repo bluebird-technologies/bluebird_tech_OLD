8ea2be81c296e641656673f5af7b91567961bdd1
marvelous-ikechi <mikechi@bscglobal.com>
2023-04-19 13:19:44 +0100

    Form card responsiveness updated

--- components/Calculator/FormCard.tsx
+++ components/Calculator/FormCard.tsx
@@ -36,18 +36,18 @@ function FormCard({
 
   return (
     <div className="flex flex-col">
-      <h3 className="text-white text-4xl font-semibold">{title}</h3>
-      <div className="mt-8">
-        <span className="text-white text-2xl">{description}</span>
+      <h3 className="text-white text-center text-3xl mx-2 xl:text-4xl font-semibold">{title}</h3>
+      <div className="mt-8 flex  items-center justify-center px-2">
+        <span className="text-white text-center text-lg xl:text-2xl">{description}</span>
       </div>
       <div
         className={
           alignOptionsLeft
             ? 'flex flex-row mt-16 justify-start space-x-24'
-            : 'flex flex-row mt-16 justify-between'
+            : 'inline-grid grid-cols-2 mt-8 gap-4 xl:flex xl:flex-row xl:mt-16 xl:justify-between'
         }
       >
-        {options.map((option) => (
+        {options.map((option, index) => (
           <div
             onClick={() =>
               setSelectedOption({
@@ -55,23 +55,27 @@ function FormCard({
               })
             }
             key={option.optionIndex}
-            className="cursor-pointer justify-center items-center text-center"
+            className={`cursor-pointer mx-2 my-8 flex flex-col justify-center items-center text-center ${
+              index === options.length - 1 && options.length % 2 !== 0 ? 'col-span-2' : ''
+            }`}
           >
-            <div className="h-[120px] transition transform hover:scale-125 duration-300">
+            <div className="xl:h-[120px] xl:w-auto w-[80px] transition mx-2 transform hover:scale-125 duration-300">
               {option.icon}
             </div>
-            <div className="text-center text-white font-medium text-xl mt-6">{option.title}</div>
+            <div className="text-center text-white font-medium text-xl  mt-6 ">{option.title}</div>
           </div>
         ))}
       </div>
       {backButton && goBack && (
-        <div className="w-full flex justify-between mt-12">
+        <div className="w-full flex justify-center items-center  mt-12">
           <button
             onClick={() => goBack()}
-            className="text-white font-medium text-xl flex items-center justify-center"
+            className="text-white font-medium xl:text-xl flex items-center justify-center"
           >
-            <RightArrow className="origin-center rotate-180 mr-4" />
-            BACK
+            <div className="flex items-center justify-center xl:h-auto xl:w-auto ">
+              <RightArrow className="origin-center rotate-180 xl:mr-4" />
+            </div>
+            <h1 className="ml-8 xl:ml-0">BACK</h1>
           </button>
         </div>
       )}
