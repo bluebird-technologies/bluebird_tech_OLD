de9c365efd17e36bee89dc58a41356f523781b6e
marvelous-ikechi <mikechi@bscglobal.com>
2023-04-20 14:19:19 +0100

    Team carousel created

---
+++ components/Carousel/TeamCarousel.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Lottie from 'lottie-react';
+import teammemberanimation from '../../public/lottie/teammemberanimation.json';
+
+interface Props {
+  item: string[];
+}
+
+export default function TeamCarousel({ item }: Props) {
+  return (
+    <Carousel
+      emulateTouch
+      infiniteLoop
+      showArrows={true}
+      showStatus={false}
+      showIndicators={false}
+      showThumbs={true}
+      width={350}
+      centerMode={true}
+      centerSlidePercentage={100}
+      onChange={(val) => console.log(val)}
+    >
+      {item?.map((label, i) => {
+        return (
+          <div className="flex items-center  justify-center" key={i}>
+            <div className="flex justify-center    items-center">
+              <CarouselSlideItem label={label} />
+            </div>
+          </div>
+        );
+      })}
+    </Carousel>
+  );
+}
+
+interface CarouselSlideItemProps {
+  label: string;
+}
+
+function CarouselSlideItem({ label }: CarouselSlideItemProps) {
+  return (
+    <div className="flex  h-full flex-col items-center justify-between">
+      <div className="flex justify-center text-center items-center">
+        <Lottie className="h-[300px]" animationData={teammemberanimation} loop={true} />
+      </div>
+      <div className="text-4xl text-white font-medium text-center">{label}</div>
+    </div>
+  );
+}
