b422446ad8bf2090f7c268d3b0c8f428a7f96955
marvelous-ikechi <mikechi@bscglobal.com>
2023-04-27 16:59:45 +0100

    mobile header updated to fit designs

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -16,36 +16,55 @@ export function MobileHeader() {
   return (
     <>
       {showNavigation && (
-        <div className="fixed bg-primary flex w-full h-[100vh] p-4" style={{ zIndex: 50 }}>
-          <div className="flex flex-col w-[40vw]">
-            {navLinks.map((nav) => {
-              return (
-                <div key={'nav-' + nav.label} className="py-1">
-                  <Link className="text-white font-semibold hover:font-bold" href={nav.href}>
-                    {nav.label}
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-          <div className="absolute right-0 px-2">
-            <button
-              type="button"
-              className="text-white text-4xl"
-              onClick={() => setShowNavigation((prev) => !prev)}
-            >
-              X
-            </button>
+        <div className="fixed  bg-primary flex w-full h-[100vh] p-4" style={{ zIndex: 50 }}>
+          <div className="flex w-full flex-col">
+            <div className="flex  w-full    items-center justify-between flex-row">
+              <picture>
+                <img className="h-32  w-32" src="/header/mobile-logo.svg" alt="logo" />
+              </picture>
+              <div className=" right-0 px-2">
+                <button
+                  type="button"
+                  className="text-white text-4xl"
+                  onClick={() => setShowNavigation((prev) => !prev)}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-1 w-full items-center justify-center flex-col ">
+              {navLinks.map((nav) => {
+                return (
+                  <div key={'nav-' + nav.label} className="py-1 mb-8">
+                    <Link
+                      className="text-white text-3xl font-semibold hover:font-bold"
+                      href={nav.href}
+                    >
+                      {nav.label}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       )}
+
       {!showNavigation && (
         <div
-          className="fixed w-full flex justify-end p-2 h-[60px] bg-primary border-b border-secondary"
+          className=" w-full flex  p-2 h-[80px] items-center  justify-between bg-inherit border-b border-secondary"
           style={{ zIndex: 50 }}
         >
+          <picture>
+            <img className="h-32 w-32" src="/header/mobile-logo.svg" alt="logo" />
+          </picture>
           <div className="text-white">
-            <button type="button" className="text-white w-12 h-12" onClick={() => handleShowMenu()}>
+            <button
+              title="menu"
+              type="button"
+              className="text-white w-12 h-12"
+              onClick={() => handleShowMenu()}
+            >
               <MenuIcon />
             </button>
           </div>
